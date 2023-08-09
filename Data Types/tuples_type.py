07dc4fed511f1e3e80a45580cc62196e79472429
gnicezw <gnice5@gmail.com>
2023-08-09 19:50:39 +0800

    general update

---
+++ Data Types/tuples_type.py
@@ -0,0 +1,7 @@
+#this is a regular tuple
+nebula_tuple = ("orion","eagle", "ring", "helix")
+print(nebula_tuple)
+
+#declaring a tuple with only 1 item
+planet_tuple = ("Jupiter",)
+print(planet_tuple)
---
+++ Functions/Functions_keyword_args.py
@@ -0,0 +1,4 @@
+def celestial_objects(celestial1, celestial2, celestial3, celestrial4):
+    print("{}s are deep in outer space".format(celestial3))
+
+celestial_objects(celestial2="Black hole", celestrial4="SuperNova", celestial1="Neutron Star", celestial3="gas giant")
